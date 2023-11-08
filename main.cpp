331717f5f70ef9fd8ecf7a1142b78279853eaf39
aliakseis <aliaksei.sanko@gmail.com>
2023-11-08 10:39:54 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,114 +20,6 @@
 #include <iostream>
 
 
-namespace {
-
-#if 0
-
-// Parses the value of the active python exception
-// NOTE SHOULD NOT BE CALLED IF NO EXCEPTION
-std::string parse_python_exception()
-{
-    namespace py = pybind11;
-
-    PyObject *type_ptr = NULL, *value_ptr = NULL, *traceback_ptr = NULL;
-    // Fetch the exception info from the Python C API
-    PyErr_Fetch(&type_ptr, &value_ptr, &traceback_ptr);
-
-    // Fallback error
-    std::string ret("Unfetchable Python error");
-    // If the fetch got a type pointer, parse the type into the exception string
-    if(type_ptr != NULL){
-        try {
-            py::handle h_type(type_ptr);
-            py::str type_pstr(h_type);
-            ret = type_pstr;
-        }
-        catch (...) {
-            ret = "Unknown exception type";
-        }
-    }
-    // Do the same for the exception value (the stringification of the exception)
-    if(value_ptr != NULL){
-        try {
-            py::handle h_val(value_ptr);
-            py::str a(h_val);
-            ret += ": " + std::string(a);
-        }
-        catch (...) {
-            ret += ": Unparseable Python error: ";
-        }
-    }
-    // Parse lines from the traceback using the Python traceback module
-    if(traceback_ptr != NULL){
-        py::handle h_tb(traceback_ptr);
-        // Load the traceback module and the format_tb function
-        py::object tb(py::import("traceback"));
-        py::object fmt_tb(tb.attr("format_tb"));
-        // Call format_tb to get a list of traceback strings
-        py::object tb_list(fmt_tb(h_tb));
-        // Join the traceback strings into a single string
-        py::object tb_str(py::str("\n").join(tb_list));
-        // Extract the string, check the extraction, and fallback in necessary
-        py::extract<std::string> returned(tb_str);
-        if(returned.check())
-            ret += ": " + returned();
-        else
-            ret += ": Unparseable Python traceback";
-    }
-    return ret;
-}
-
-
-// convert a cv::mat to an np.array
-py::array to_array(const cv::Mat& im) {
-    const auto channels = im.channels();
-    const auto height = im.rows;
-    const auto width = im.cols;
-    const auto dim = sizeof(uchar) * height * width * channels;
-    auto data = new uchar[dim];
-    std::copy(im.data, im.data + dim, data);
-    return py::array_t<uchar>(
-        py::buffer_info(
-            data,
-            sizeof(uchar), //itemsize
-            py::format_descriptor<uchar>::format(),
-            channels, // ndim
-            std::vector<ssize_t> { height, width, channels }, // shape
-            std::vector<ssize_t> { width* channels, channels, sizeof(uchar) } // strides
-    ),
-        py::capsule(data, [](void* f) {
-            // handle releasing data
-            delete[] reinterpret_cast<uchar*>(f);
-            })
-    );
-}
-
-
-// convert an np.array to a cv::mat
-cv::Mat from_array(const py::array& ar) {
-    if (!ar.dtype().is(py::dtype::of<uchar>())) {
-        std::cout << "error unsupported dtype!" << std::endl;
-        return cv::mat();
-    }
-
-    auto shape = ar.shape();
-    int rows = shape[0];
-    int cols = shape[1];
-    int channels = shape[2];
-    int type = cv_maketype(cv_8u, channels); // cv_8uc3
-    cv::mat mat = cv::mat(rows, cols, type);
-    memcpy(mat.data, ar.data(), sizeof(uchar) * rows * cols * channels);
-
-    return mat;
-}
-
-#endif
-
-
-} // namespace
-
-
 
 const cv::String keys =
     "{help h usage ? |      | print this message   }"
@@ -194,9 +86,6 @@ int main(int argc, char **argv)
 
         return TransformVideo(input.c_str(), output.c_str(), lam, 1, downscale);
     }
-    //catch (const py::error_already_set& ex) {
-    //    std::cerr << "Exception " << typeid(ex).name() << ": " << py::detail::error_string() << '\n';
-    //}
     catch (const std::exception& ex) {
         std::cerr << "Exception " << typeid(ex).name() << ": " << ex.what() << '\n';
         return EXIT_FAILURE;
